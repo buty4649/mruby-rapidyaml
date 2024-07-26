4abb7d357206b5392ec52452f62334acc05a5a0b
buty4649 <buty4649@gmail.com>
2024-07-27 01:48:08 +0900

    Add colorize option

--- mrblib/yaml.rb
+++ mrblib/yaml.rb
@@ -1,11 +1,28 @@
 module YAML
   class AliasesNotEnabled < StandardError; end
   class AnchorNotDefined < StandardError; end
+  class GeneratorError < StandardError; end
   class SyntaxError < StandardError; end
 
-  if Object.const_defined?(:IO)
-    def self.load_file(filename, opts = {})
-      YAML.load(IO.read(filename), opts)
+  class << self
+    if Object.const_defined?(:IO)
+      def load_file(filename, opts = {})
+        YAML.load(IO.read(filename), opts)
+      end
     end
+
+    def color_object_key
+      @color_object_key ||= :blue
+    end
+
+    def color_string
+      @color_string ||= :green
+    end
+
+    def color_null
+      @color_null ||= :gray
+    end
+
+    attr_writer :color_object_key, :color_string, :color_null
   end
 end
---
+++ src/mrb_terminal_color.h
@@ -0,0 +1,11 @@
+#ifndef _MRB_TERMINAL_COLOR_H_
+#define _MRB_TERMINAL_COLOR_H_
+
+#include <mruby.h>
+
+extern "C"
+{
+    mrb_value mrb_str_set_color(mrb_state *mrb, mrb_value str, mrb_value color, mrb_value bg_color, mrb_value mode);
+}
+
+#endif // _MRB_TERMINAL_COLOR_H_
